be208737bcde9656ffeffbfbd14fe9b4172a4ac7
abonil3 <alex45655@gmail.com>
2019-07-16 21:21:22 -0500

    Getting possible moves
    added function to get possible moves for moving empty space, as well as the tests needed for that function

--- Slide_Puzzle/nine_tile.py
+++ Slide_Puzzle/nine_tile.py
@@ -1,4 +1,5 @@
 import random
+import itertools
 """
 the board will have 9 blocks that have a number 1-8 with the 9th block having no number. These
 blocks will be arranged in random order in a 3x3 board. the point of the game is to have 
@@ -26,15 +27,24 @@ rules:
 MBL = 9 # 0 index Maximum Board Length
 class Board(object):
     def __init__(self):
+        self.empty_space = None
         self.board = self.setup_board()
 
     def setup_board(self):
+        """Creates and populates the board
+        
+        Returns:
+            list[Tile] -- list with MBL x MBL lists each containing a Tile Object
+        """
         board_rows = []
         random_nums = random.sample(range(MBL),9)
-        for x in range(MBL//3):
+        for y in range(MBL//3):
             row = []
-            for y in range(MBL//3):
-                row.append(Tile(x,y,random_nums.pop(0)))
+            for x in range(MBL//3):
+                number = random_nums.pop(0)
+                if number == 0:
+                    self.empty_space =[y,x]
+                row.append(Tile(x,y,number))
             board_rows.append(row)
         return board_rows
        
@@ -44,7 +54,25 @@ class Board(object):
 
     
     def possible_moves(self):
-        pass
+        possible_x = []
+        possible_y = []
+        if 0 < self.empty_space[1]<(MBL//3)-1:
+            possible_x.append(self.empty_space[1]+1)
+            possible_x.append(self.empty_space[1]-1)
+        elif self.empty_space[1] == (MBL//3)-1:
+            possible_x.append(self.empty_space[1]-1)
+        else:
+            possible_x.append(self.empty_space[1]+1)
+        if 0 < self.empty_space[0] < (MBL//3)-1:
+            possible_y.append(self.empty_space[0]+1)
+            possible_y.append(self.empty_space[0]-1)
+        elif self.empty_space[0] == (MBL//3)-1:
+            possible_y.append(self.empty_space[0]-1)
+        else:
+            possible_y.append(self.empty_space[0]+1)
+        set_of_moves = [(y,self.empty_space[1]) for y in possible_y]
+        set_of_moves = set_of_moves + [(self.empty_space[0],x) for x in possible_x]
+        return set_of_moves
 
     def start_game(self):
         pass
@@ -65,7 +93,7 @@ class Tile:
         self.num = num
 
     def __repr__(self):
-        if self.num != 1:
+        if self.num != 0:
             return "[{}]".format(self.num)
         else:
             return "[ ]"
@@ -80,6 +108,7 @@ class Tile:
 if __name__ == "__main__":
     b = Board()
     b.print_board()
+    print(b.possible_moves())
     
 
 
--- Tests/test_nine_tile.py
+++ Tests/test_nine_tile.py
@@ -12,13 +12,55 @@ from nine_tile import Board,Tile, MBL
 
 class Test_Board(unittest.TestCase):
 
-    # @patch(Board, '__init__',return_value=None) as mock_board:
     def test_setup_board(self):
+        """Test to see if the size of the board that is created is MBL x MBL
+        """
         with patch.object(Board, '__init__', lambda x: None), patch.object(nine_tile,'Tile', return_value=[]):
             b = Board()
             board = b.setup_board()
-            test_b = [[[],[],[]],[[],[],[]],[[],[],[]]]
+            test_b = [[[] for y in range(MBL//3)] for x in range(MBL//3)]
             self.assertEqual(board, test_b)
+
+    @patch.object(Board, '__init__', lambda x: None)
+    def test_possible_moves_top_left(self):
+        b = Board()
+        b.empty_space = [0,0]
+        result = set(b.possible_moves())
+        possible_moves = set([(0,1),(1,0)])
+        self.assertEqual(result,possible_moves)
+
+    @patch.object(Board, '__init__', lambda x: None)
+    def test_possible_moves_botom_left(self):
+        b = Board()
+        b.empty_space = [2,0]
+        result = set(b.possible_moves())
+        possible_moves = set([(1,0),(2,1)])
+        self.assertEqual(result,possible_moves)
+
+    @patch.object(Board, '__init__', lambda x: None)
+    def test_possible_moves_top_right(self):
+        b = Board()
+        b.empty_space = [0,2]
+        result = set(b.possible_moves())
+        possible_moves = set([(0,1),(1,2)])
+        self.assertEqual(result,possible_moves)
+
+    @patch.object(Board, '__init__', lambda x: None)
+    def test_possible_moves_botom_right(self):
+        b = Board()
+        b.empty_space = [2,2]
+        result = set(b.possible_moves())
+        possible_moves = set([(1,2),(2,1)])
+        self.assertEqual(result,possible_moves)
+
+    @patch.object(Board, '__init__', lambda x: None)
+    def test_possible_moves_center(self):
+        b = Board()
+        b.empty_space = [1,1]
+        result = set(b.possible_moves())
+        possible_moves = set([(0,1),(2,1),(1,0),(1,2)])
+        self.assertEqual(result,possible_moves)
+
 
 
 if __name__ == "__main__":
